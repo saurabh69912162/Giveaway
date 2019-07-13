4d13de4e4afbd70c8de222b69a1e44074c31e67f
Saurabh Saxena <saurabh69912162@gmail.com>
2019-07-13 10:50:35 +0530

    Update tests.py

--- details/tests.py
+++ details/tests.py
@@ -34,24 +34,21 @@ def set_me(count):
 
 
     if count == 3:
+
+
         answer = []
         answer = set(obj['0']).intersection(obj['1'],obj['2'])
         print(answer)
         print(len(answer))
         for xyz in answer:
-            for i in range(len(answer)):
+            for i in range(0,len(answer)):
                 doit = zip(obj[str(i)],profile_url[str(i)], comment[str(i)])
                 for x, y, z in doit:
                     if xyz in x:
                         print(x)
-                        print(y)
                         print(z)
 
-                        print('$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%')
-                        print('$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%')
-                        print(i)
-                        print('$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%')
-                        print('$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%')
+                        break
 
     if count == 4:
         print(set(obj['0']).intersection(obj['0'],obj['1'],obj['2'],obj['3']))
@@ -101,7 +98,7 @@ url = 'https://www.youtube.com/watch?v=KoZM4M9U7GM'
 url1 = 'https://www.youtube.com/watch?v=giSFVAjrpmA'
 url2 = 'https://www.youtube.com/watch?v=7nMYcZ-0V1o'
 
-vids = [url,url1,url]
+vids = [url,url1,url2]
 all_comments = []
 
 obj = {}
