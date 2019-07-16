f299d5e0d12ebf0a49175b17e0dcdc9fafd53796
Saurabh Saxena <saurabh69912162@gmail.com>
2019-07-16 14:18:27 +0530

    model commit 1

--- details/models.py
+++ details/models.py
@@ -120,3 +120,7 @@ class giveaway_analytics(models.Model):
     completed = models.IntegerField(default=0)
     partial = models.IntegerField(default=0)
 
+class comments(models.Model):
+    name = models.CharField(max_length=255,blank=True)
+    comment = models.CharField(max_length=5500, blank=True)
+    url = models.CharField(max_length=500)
